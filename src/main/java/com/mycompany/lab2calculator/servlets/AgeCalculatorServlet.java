4589213fbbb65d30b2f830a127e24a12e0aa8a4d
Meron Seyoum <meryato@gmail.com>
2022-01-17 21:01:12 -0700

    Age form feature is add

---
+++ src/main/java/com/mycompany/lab2calculator/servlets/AgeCalculatorServlet.java
@@ -0,0 +1,45 @@
+package com.mycompany.lab2calculator.servlets;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Meron Seyoum
+ */
+public class AgeCalculatorServlet extends HttpServlet {
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp").forward(request, response);
+ 
+    }
+
+   
+}
---
+++ target/Lab2Calculator-1.0-SNAPSHOT/WEB-INF/ageCalculator.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : agecalculator
+    Created on : 17-Jan-2022, 8:38:37 PM
+    Author     : merya
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Age Calculator</title>
+    </head>
+    <body>
+        <h1>Age Calculator</h1>
+    </body>
+</html>
